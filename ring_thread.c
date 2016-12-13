18fb2a69778360c6758462f9fee75ad4d3db6a64
Steven <steven.chien@connect.polyu.hk>
2016-12-13 16:21:19 +0100

    Added comments

--- ring_thread.c
+++ ring_thread.c
@@ -5,50 +5,49 @@
 #include <pthread.h>
 #include <unistd.h>
 
+/* global barrier and debug flag */
 pthread_barrier_t barrier;
 int debug;
 
+/* structure for placeholders between threads */
 typedef struct synch_t {
 	int token;
 	pthread_mutex_t mtx;
 } synch_t;
 
+/* structure for arguments to thread callback */
 typedef struct proc_arg_t {
 	int id;
 	int p;
 	int r;
+
+	/* pointer to the left and right placeholder */
 	synch_t *left;
 	synch_t *right;
 } proc_arg_t;
 
-void timespec_diff(struct timespec *start, struct timespec *stop, struct timespec *result)
-{
-	if ((stop->tv_nsec - start->tv_nsec) < 0) {
-		result->tv_sec = stop->tv_sec - start->tv_sec - 1;
-		result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
-	} else {
-		result->tv_sec = stop->tv_sec - start->tv_sec;
-		result->tv_nsec = stop->tv_nsec - start->tv_nsec;
-	}
-
-	return;
-}
-
+/* thread callback */
 void *ring_process(void *arg)
 {
+	/* extract id info */
 	int rounds = ((proc_arg_t*)arg)->r;
 	int id = ((proc_arg_t*)arg)->id;
 
+	/* extract pointer to token placeholder in left and right */
 	int *prev_token = &(((proc_arg_t*)arg)->right->token);
 	int *next_token = &(((proc_arg_t*)arg)->left->token);
 
+	/* extract mutex lock of placeholder from left and right */
 	pthread_mutex_t *prev_mtxp = &(((proc_arg_t*)arg)->right->mtx);
 	pthread_mutex_t *next_mtxp = &(((proc_arg_t*)arg)->left->mtx);
 
 	if(debug)
 		fprintf(stderr, "thread %d started\n", id);
+
+	/* wait for all other threads before begin */
 	pthread_barrier_wait(&barrier);
 
+	/* run for pre defined rounds */
 	while(rounds>0) {
 		/* acquire mutex of token */
 		pthread_mutex_lock(prev_mtxp);
@@ -56,14 +55,15 @@ void *ring_process(void *arg)
 		/* check if token is there */
 		if(debug)
 			fprintf(stderr, "I'm %d in round %d, token is %d, I received %d, ", id, rounds, *next_token, *prev_token);
-		*next_token = *prev_token + 1;
+	
+		*next_token = *prev_token + 1;	// work with token before passing on
 
 		/* remove token and unlock previous */
 		*prev_token = -1;
 		if(debug)
 			fprintf(stderr, "Exchange performed, now token is %d, prev token is %d, unlock next thread!\n", *next_token, *prev_token);
 		
-		/* unlock and signal on conditional */
+		/* unlock mutex of next placeholder to signal the next thread */
 		pthread_mutex_unlock(next_mtxp);
 
 		rounds--;
@@ -96,7 +96,7 @@ double benc(int p, int r)
 		placeholders[i]->token = -1;
 
 		pthread_mutex_init(&(placeholders[i]->mtx), NULL);
-		pthread_mutex_lock(&(placeholders[i]->mtx));
+		pthread_mutex_lock(&(placeholders[i]->mtx));	// lock all mutex so all processes will be waiting except the one that's active
 	}
 
 	/* init thread arguments */
@@ -105,6 +105,7 @@ double benc(int p, int r)
 		arg[i]->p = p;
 		arg[i]->id = i;
 
+		/* setup pointers to placeholders for each thread */
 		if(i==p-1) {
 			arg[i]->left = placeholders[0];
 		}
@@ -116,8 +117,8 @@ double benc(int p, int r)
 	}
 
 	/* take lock of one placeholder */
-	placeholders[0]->token = 0;
-	pthread_mutex_unlock(&(placeholders[0]->mtx));
+	placeholders[0]->token = 0;	// init the token from the first placeholder
+	pthread_mutex_unlock(&(placeholders[0]->mtx));	// unlock the first mutex so that it starts from the first thread
 
 	/* create all thread */
 	for(int i=0; i<p; i++) {
@@ -165,6 +166,7 @@ int main(int argc, char *argv[])
 	int rounds=5, thread_count=5, c;
 	debug = 0;
 
+	/* extract cmd arguments */
 	while((c=getopt(argc, argv, "dp:r:")) != -1) {
 		switch (c) {
 			case 'd':
